23598d3b874d23c9ff7a8c051bb8b023ce1313fd
mondrus <mondrus@ualberta.ca>
2024-01-16 16:08:52 -0700

    added more processes for slurm

--- Experiments/sim_circ.sh
+++ Experiments/sim_circ.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-ijc               # account associated with job
 #SBATCH --nodes=1                       # number of node MUST be 1
-#SBATCH --cpus-per-task=4               # number of processes
+#SBATCH --cpus-per-task=16              # number of processes
 #SBATCH --mem=0                         # memory usage
 #SBATCH --time=0-05:59                  # time (DD-HH:MM)
 #SBATCH --mail-user=mondrus@ualberta.ca # Send email updates to you or someone else
