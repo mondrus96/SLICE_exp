34cd5e8b11b2e3be54e1961975c9a33131b7aea9
mondrus <mondrus@ualberta.ca>
2024-01-16 16:13:15 -0700

    testing parallelization

---
+++ Experiments/testpar.R
@@ -0,0 +1,13 @@
+# Uniform cluster sizes
+sapply((paste0("../Models/", list.files("../Models/"))), source)
+sapply((paste0("../Simulations/", list.files("../Simulations/"))), source)
+
+set.seed(123)
+pobs <- 200 # Number of observed variables for S
+plat <- 2 # Number of latent variables for L
+ns <- seq(100, 150, 50) # Number of observations
+
+init_S <- 1.5 # Initial value for S
+
+simtype <- "circ"
+runsim.par(simtype, pobs, plat, ns, init_S, init_L)
