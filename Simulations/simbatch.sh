8b0001ce886a95059f8cd4d9507ed4e511390b5c
mondrus <mondrus@ualberta.ca>
2024-01-19 23:55:43 -0700

    for testing

--- Simulations/simbatch.sh
+++ Simulations/simbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                       # number of node MUST be 1
 #SBATCH --cpus-per-task=1               # number of processes
 #SBATCH --mem=0                         # memory usage
-#SBATCH --time=0-09:59                  # time (DD-HH:MM)
+#SBATCH --time=0-30:00                  # time (DD-HH:MM)
 #SBATCH --mail-user=mondrus@ualberta.ca # Send email updates to you or someone else
 #SBATCH --mail-type=ALL                 # Send an email in all cases (job started, job ended, job aborted)
 
