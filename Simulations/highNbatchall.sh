9fac378f1e65512cee352dc7723f7c93bde986d0
mondrus <mondrus@ualberta.ca>
2024-07-08 11:45:09 +0900

    changed loops

--- Simulations/highNbatchall.sh
+++ Simulations/highNbatchall.sh
@@ -3,9 +3,9 @@ models=("SLICE" "SLICE_GSCAD" "SLICE_CLIME" "nnLVGLASSO" "rcLVGLASSO")
 simtypes=("cres" "spir" "rand" "eeg")
 
 # Loop over models
-for model in models; do
+for model in "${models[@]}"; do
     # Loop over simtypes
-    for simtype in simtypes; do
+    for simtype in "${simtypes[@]}"; do
         # Submit the job to SLURM with Rscript arguments
         sbatch --job-name="sim${simtype}_model${model}" --time=0-11:59 highNbatch.sh $model $simtype
         sleep 1
