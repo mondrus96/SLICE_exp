531e33107f438e3711ac4bd70b7beb238b7ca74a
mondrus <mondrus@ualberta.ca>
2024-07-09 22:34:37 +0900

    removed email updates

--- Simulations/highNbatch_.sh
+++ Simulations/highNbatch_.sh
@@ -3,7 +3,6 @@
 #SBATCH --nodes=1                       # number of node MUST be 1
 #SBATCH --cpus-per-task=1               # number of processes
 #SBATCH --mem=0                         # memory usage
-#SBATCH --mail-user=mondrus@ualberta.ca # Send email updates to you or someone else
 #SBATCH --mail-type=ALL                 # Send an email in all cases (job started, job ended, job aborted)
 
 module load StdEnv/2023 gcc/13.3 r/4.4.0
